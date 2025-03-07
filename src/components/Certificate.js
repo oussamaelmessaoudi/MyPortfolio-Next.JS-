7aa1cc781319f97d4f45db36f884edfb3ca2ab2f
unknown <oussamaelmessaoudi17@gmail.com>
2025-03-07 17:19:31 +0000

    new commit

--- src/components/Certificate.js
+++ src/components/Certificate.js
@@ -25,6 +25,20 @@ export function Certificate() {
         designation: "Kiggle",
         src: "/Python.png",
       },
+      {
+        quote:
+          "This certification validates foundational Adobe Photoshop skills, including basic image editing, layer management, selection tools, color correction, and text manipulation. It demonstrates proficiency in creating and modifying visual content using Adobe Photoshop, showcasing the ability to work with essential tools and techniques for graphic design and photo enhancement.",
+        name: "Adobe Photoshop for beginners",
+        designation: "Alison",
+        src: "/Photoshop.jpg",
+      },
+      {
+        quote:
+          "This certification validates fundamental Google Sheets skills, including data organization, formulas, pivot tables, conditional formatting, and data visualization. It demonstrates proficiency in automating workflows, managing large datasets, and creating dynamic reports using Google Sheets' advanced features. This certification showcases the ability to streamline data processes and improve efficiency through automation and optimized spreadsheet management.",
+        name: "Google Sheets",
+        designation: "Simplilearn",
+        src: "/Google Sheets.jpg",
+      },
   
 
   ];
