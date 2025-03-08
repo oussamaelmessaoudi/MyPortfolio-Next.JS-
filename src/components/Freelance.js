33f831950e5c747f7ffc25a095163eb9559ab5d2
unknown <oussamaelmessaoudi17@gmail.com>
2025-03-08 19:46:10 +0000

    new commit

--- src/components/Freelance.js
+++ src/components/Freelance.js
@@ -21,6 +21,7 @@ const projects = [
       { type: "image", url: "/Shopify.jpg", caption: "Shopify Thumbnail" },
       { type: "image", url: "/Story-Time.jpg", caption: "Story Time Thumbnail" },
       { type: "image", url: "/Brazil.jpg", caption: "Vlog Thumbnail" },
+      { type: "image", url: "/Make-money-online.jpg", caption: "Making Money Thumbnail" },
     ]
   },
   // Add other projects...
